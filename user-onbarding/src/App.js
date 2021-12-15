b03a9f90f7462d7b124850c545d2a360dfe4b46a
Winifred Lu <winnie.lu@comcast.net>
2021-12-15 14:16:57 -0500

    initial app setup

---
+++ user-onbarding/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Form from './components/Form';
+
+function App() {
+  return (
+    <div className="container">
+      <header><h1>User App</h1></header>
+      
+      <Form
+
+      />
+    </div>
+  );
+}
+
+export default App;
