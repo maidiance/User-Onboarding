366ec4e0e100c9b7cc0019614c1136c0c0a7dfcf
Winifred Lu <winnie.lu@comcast.net>
2021-12-16 15:15:00 -0500

    terms of service test working

--- user-onbarding/cypress/integration/form_test.js
+++ user-onbarding/cypress/integration/form_test.js
@@ -7,14 +7,14 @@ describe('User App', () => {
     const nameInput = () => cy.get('input[name=first_name');
     const emailInput = () => cy.get('input[name=email]');
     const passwordInput = () => cy.get('input[name=password]');
-    const termsOfServiceInput = () => cy.get('input[type="checkbox"]');
+    const tosInput = () => cy.get('input[type="checkbox"]');
     const submitButton = () => cy.get('button[id="submitBtn"]');
 
     it('proper elements are showing', () => {
         nameInput().should('exist');
         emailInput().should('exist');
         passwordInput().should('exist');
-        termsOfServiceInput().should('exist');
+        tosInput().should('exist');
         submitButton().should('exist');
     })
 
@@ -42,6 +42,17 @@ describe('User App', () => {
                 .should('have.value', '')
                 .type('testpassword')
                 .should('have.value', 'testpassword');
+        })
+    })
+
+    describe('Check terms of service functionality', () => {
+        it('can check terms of service checkbox', () => {
+            tosInput().should('not.be.checked');
+            tosInput().check();
+            tosInput().should('be.checked');
+            // reset back to baseline
+            tosInput().check();
+            tosInput().should('not.be.checked');
         })
     })
 })
